d45441d250f524a864e22e6b101f312037c9ad8b
Alex Malyutin <malutin.alex@gmail.com>
2019-03-14 20:59:15 +0100

    FSN class

---
+++ NetEmbs/FSN/graph.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+__author__ = 'Aleksei Maliutin'
+"""
+graph.py
+Last modified by lex at 2019-03-14.
+"""
+import networkx as nx
+
+
+class FSN(nx.DiGraph):
+    """
+    Financial Statement Network class. Includes construction, projection, plotting methods
+    """
+
+    def __init__(self, df):
+        """
+        Construct Financial Statement Network (FSN) from DataFrame
+        :param df: DataFrame with JournalEntities
+        """
+        super().__init__()
+        self.add_nodes_from(df['ID'], bipartite=0)
+        self.add_nodes_from(df['Name'], bipartite=1)
+        self.add_weighted_edges_from(
+            [(row['Name'], row['ID'], row["Credit"]) for idx, row in df[df["from"] is True].iterrows()],
+            weight='weight', type="CREDIT")
+        self.add_weighted_edges_from(
+            [(row['ID'], row['Name'], row["Debit"]) for idx, row in df[df["from"] is False].iterrows()],
+            weight='weight', type="DEBIT")
