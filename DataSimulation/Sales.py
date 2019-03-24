6d1d32892e43073cea0fd87acff3e5a7e8ea1042
Alex Malyutin <malutin.alex@gmail.com>
2019-03-24 18:09:12 +0100

    ..

--- DataSimulation/Sales.py
+++ DataSimulation/Sales.py
@@ -4,75 +4,64 @@ from Event import *
 
 import numpy as np
 
-
 import random
+
 
 class SalesTransaction(Transaction):
 
-	def __init__(self, name, btw, env):
-		Transaction.__init__(self, name, env)
-		self.btw = btw
-		self.name = name
-		self.debug = False
+    def __init__(self, name, btw, env):
+        Transaction.__init__(self, name, env)
+        self.btw = btw
+        self.name = name
+        self.debug = False
 
-	def newTransaction(self):
-		#retrieve new transaction ID
-		transaction = self.new()
+    def newTransaction(self):
+        # retrieve new transaction ID
+        transaction = self.new()
 
-		#generate amounts
-		rev = random.randint(10,1000)
-		tax = rev*self.btw
-		tr 	= rev+tax
+        # generate amounts
+        # TODO add type1 noise here
+        rev = random.randint(10, 1000)
+        tax = rev * self.btw
+        tr = rev + tax
 
-		#save records
-		self.addRecord("Revenue", -rev, 0,  transaction)
-		self.addRecord("Tax", -tax, 0,  transaction)
-		self.addRecord("Trade receivables", tr, 0, transaction)
+        # save records
+        self.addRecord("Revenue", -rev, 0, transaction)
+        self.addRecord("Tax", -tax, 0, transaction)
+        self.addRecord("Trade receivables", tr, 0, transaction)
 
+        c1_f = np.random.binomial(1, 1 - max(min(1, 0.3 + random.gauss(0, 0.01)), 0), 1)[0]
+        c2_f = np.random.binomial(1, 1 - max(min(1, 0.3 + random.gauss(0, 0.01)), 0), 1)[0]
+        c3_f = np.random.binomial(1, 1 - max(min(1, 0.3 + random.gauss(0, 0.01)), 0), 1)[0]
 
-		c1_f = np.random.binomial(1,  1-max(min(1, 0.3 + random.gauss(0, 0.01)), 0), 1)[0]
-		c2_f = np.random.binomial(1,  1-max(min(1, 0.3 + random.gauss(0, 0.01)), 0), 1)[0]
-		c3_f = np.random.binomial(1,  1-max(min(1, 0.3 + random.gauss(0, 0.01)), 0), 1)[0]
+        self.printTransaction(tr, rev, tax)
 
-		self.printTransaction(tr, rev, tax)
+        return [tr * c1_f, tr * (1 - c1_f), rev * c2_f, rev * (1 - c2_f), tax * c3_f, tax * (1 - c3_f), self.name]
 
-		return [tr*c1_f, tr*(1-c1_f),  rev*c2_f, rev*(1-c2_f),  tax*c3_f, tax*(1-c3_f), self.name]
-
-	def printTransaction(self, tr, rev, tax):
-		if self.debug:
-			print "Trade receivables \t %d \n@Revenue \t\t\t %d \n@Tax \t\t\t\t %d \n" % (tr, rev, tax)
-
-
+    def printTransaction(self, tr, rev, tax):
+        if self.debug:
+            print "Trade receivables \t %d \n@Revenue \t\t\t %d \n@Tax \t\t\t\t %d \n" % (tr, rev, tax)
 
 
 class SalesProcess(Process):
 
-	def __init__(self, env, name, transaction):
-		print "Process %s" % name
+    def __init__(self, env, name, transaction):
+        print "Process %s" % name
 
-		self.name 						= name
-		self.aTransaction 				= transaction
-		self.env 						= env
+        self.name = name
+        self.aTransaction = transaction
+        self.env = env
 
-		self.transactionNotifier		= Process.TransactionNotifier(self)
-		self.transactionNotifee 		= Process.TransactionNotifee(self)
-		self.lastTransactionDetails		= None
+        self.transactionNotifier = Process.TransactionNotifier(self)
+        self.transactionNotifee = Process.TransactionNotifee(self)
+        self.lastTransactionDetails = None
 
+    def randomTransactions(self, runs, col):
+        while True:
+            yield self.env.timeout(random.expovariate(1 / 4.0))
+            self.lastTransactionDetails = self.aTransaction.newTransaction()
+            [tr, tr_w, rev, rev_w, tax, tax_w, label] = self.lastTransactionDetails
 
-	def randomTransactions(self, runs, col):
-		while True:
-			yield self.env.timeout(random.expovariate(1/4.0))
-			self.lastTransactionDetails = self.aTransaction.newTransaction()
-			[tr, tr_w,  rev, rev_w,  tax, tax_w, label] = self.lastTransactionDetails
-
-
-			self.transactionNotifier.setChanged()
-			for obs in self.transactionNotifier.notifyObservers(self.lastTransactionDetails):
-				yield obs
-				
-
-
-
-
-
-
+            self.transactionNotifier.setChanged()
+            for obs in self.transactionNotifier.notifyObservers(self.lastTransactionDetails):
+                yield obs
