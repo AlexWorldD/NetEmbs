3291935740ff308beb288e85d09a913043b004e0
Alex Malyutin <malutin.alex@gmail.com>
2019-07-03 02:13:39 +0200

    Added heatmap for cross-correlation

--- NetEmbs/Vis/helpers.py
+++ NetEmbs/Vis/helpers.py
@@ -22,3 +22,43 @@ def set_font(s=16, reset=False):
     plt.rc('ytick', labelsize=s - 2)  # fontsize of the tick labels
     plt.rc('legend', fontsize=s)  # legend fontsize
     plt.rc('figure', titlesize=s)  # fontsize of the figure title
+
+
+# Transform Matplotlib colormap into plotly colorscale:
+import itertools
+import matplotlib
+import numpy as np
+
+
+def matplotlib_to_plotly(color_map="tab10", pl_entries=10):
+    """
+    Transform Matplotlib colormap into Plotly colormap
+    :param color_map: Original Matplotlib's colormap
+    :param pl_entries: number of requested colors
+    :return: Plotly color-scale
+    """
+    cmap = matplotlib.cm.get_cmap(color_map)
+    h = 1.0 / (pl_entries - 1)
+    pl_colorscale = []
+
+    for k in range(pl_entries):
+        C = list(map(np.uint8, np.array(cmap(k * h)[:3]) * 255))
+        pl_colorscale.append([k * h, 'rgb' + str((C[0], C[1], C[2]))])
+
+    return pl_colorscale
+
+
+def getColors_Markers(keys, cm="tab10", n_color=10, markers=["circle", "diamond", "square"]):
+    """
+    Construct dictionaries with the given keys and requested colors/markers. Used for deterministic behaviour of vis.
+    :param keys: The list of the given keys, e.g. unique labels or values of GroundTruth
+    :param cm: Name of colormap, Default is "tab10"
+    :param n_color: Number of colors, Default is 10
+    :param markers: The list of markers. Used if len(keys)>n_color
+    :return: Two dictionaries: with key-color and key-marker mappings.
+    """
+    keys = sorted(keys)
+    color_map = dict(zip(keys, matplotlib_to_plotly(cm, n_color) * (len(keys) // n_color + 1)))
+    marker_map = dict(
+        zip(keys, list(itertools.chain(*[[m] * n_color for m in markers])) * (len(keys) // (3 * n_color) + 1)))
+    return color_map, marker_map
