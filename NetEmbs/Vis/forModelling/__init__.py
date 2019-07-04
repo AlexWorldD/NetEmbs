eea0751bb1ed924075d5776e1e561af3e4b7ebd5
Alex Malyutin <malutin.alex@gmail.com>
2019-07-04 17:24:02 +0200

    Add some code to package.

--- NetEmbs/Vis/forModelling/__init__.py
+++ NetEmbs/Vis/forModelling/__init__.py
@@ -4,3 +4,4 @@ __author__ = 'Aleksei Maliutin'
 __init__.py.py
 Created by lex at 2019-07-03.
 """
+from NetEmbs.Vis.forModelling.CorrelationHeatMap import corrHeatmap_interactive, corrHeatmap_static
